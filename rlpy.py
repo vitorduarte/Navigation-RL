363b2587a0faea0321e8ea33d19b47b61ee22774
vitorduarte <vitor.alves.duarte@gmail.com>
2017-06-02 16:55:14 -0300

    modify the way to get next state and add a flag to simulate or not the wind

--- rlpy.py
+++ rlpy.py
@@ -30,45 +30,68 @@ class MapState(object):
         self.q_val[pos[0], pos[1]] = new_value
         self.states[pos[0], pos[1]].q_val = new_value
 
-    def get_next_state(self, row, col):
+    def get_next_state(self, row, col, wind=True):
         max_row = self.states.shape[0]
         max_col = self.states.shape[1]
-
 
         if (max_row > row) and (max_col > col):
             actual_state = self.states[row, col]
             q = round(actual_state.q_val)
 
-            #Up
-            if q == 0:
-                if row != 0:
-                    next_state = self.states[row-1, col]
-                else:
-                    next_state = actual_state
-            #Right
-            elif q == 1:
-                if col != (max_col-1):
-                    next_state = self.states[row, col+1]
-                else:
-                    next_state = actual_state
-            #Left
-            elif q == 2:
-                if col != 0:
-                    next_state = self.states[row, col-1]
-                else:
-                    next_state = actual_state
-
-            #Down
-            elif q == 3:
-                if row != (max_row-1):
-                    next_state = self.states[row+1, col]
-                else:
-                    next_state = actual_state
+            if wind==True:
+                wind_mvmt = generate_wind(row,col)
 
             else:
-                print("DIMENSION ERROR!")
+                wind_mvmt = 0
+
+            #Up
+            if q == 0:
+                movement_row = wind_mvmt-1
+                movement_col = 0
+            #Right
+            elif q == 1:
+                movement_row = wind_mvmt
+                movement_col = 1
+
+            #Down
+            elif q == 2:
+                movement_row = wind_mvmt+1
+                movement_col = 0
+
+            #Left
+            elif q == 3:
+                movement_row = wind_mvmt
+                movement_col = -1
+
+            new_row = row + movement_row
+            new_col = col + movement_col
+
+            if new_row >= max_row:
+                new_row = max_row-1
+
+            if new_row < 0:
+                new_row = 0
+
+            if new_col >= max_col:
+                new_col = max_col-1
+
+            if new_col < 0:
+                new_col = 0
+
+            next_state = self.states[new_row, new_col]
 
         else:
             next_state = -1
 
         return next_state
+
+def generate_wind(row,col):
+    if (2 < col < 6) or col == 8:
+            wind_mvmt = -1
+    elif 5 < col < 8:
+            wind_mvmt = -2
+    else:
+        wind_mvmt = 0
+
+    return wind_mvmt
+
