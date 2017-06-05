4d710c2981e6b3a1d29e46d8401782b261915280
vitorduarte <vitor.alves.duarte@gmail.com>
2017-06-05 00:42:48 -0300

    change some variable names

--- rlpy.py
+++ rlpy.py
@@ -5,54 +5,58 @@ import sys
 class State(object):
     """Class State
             @pos - postition off state in 2D
-            @rewards - rewards for 4 possible movements
+            @q_val - rewards for 4 possible movements
                         [0] - Up
                         [1] - Right
                         [2] - Down
                         [3] - Left """
     def __init__(self, reward=-1, pos=(0,0)):
         self.pos = pos
-        #self.q_val = q_val
-        self.rewards = np.empty((1,4))
-        self.rewards.fill(reward)
-        self.rewards = self.rewards.reshape(4)
+        self.reward = reward
+        self.q_val = np.empty((1,4))
+        self.q_val.fill(reward)
+        self.q_val = self.q_val.reshape(4)
 
     """@direction - string that describes what position reward change
             'u' or 0 - Up
             'r' or 1 - Right
             'd' or 2 - Down
             'l' or 3 - Left """
-    def change_reward(self, direction, new_reward):
+    def change_q_val(self, direction, new_reward):
         if (direction == 'u') or (direction == 0):
-            self.rewards[0] = new_reward
+            self.q_val[0] = new_reward
         elif (direction == 'r') or (direction == 1):
-            self.rewards[1] = new_reward
+            self.q_val[1] = new_reward
         elif (direction == 'd') or (direction == 2):
-            self.rewards[2] = new_reward
+            self.q_val[2] = new_reward
         elif (direction == 'l') or (direction == 3):
-            self.rewards[3] = new_reward
+            self.q_val[3] = new_reward
         else:
             print('INVALID DIRECTION')
 
-    def max_reward_idx(self):
-        return self.rewards.argmax()
+    def max_q_val_idx(self):
+        return self.q_val.argmax()
 
 class MapState(object):
     """docstring for Map"""
     def __init__(self, heigth, width, reward=-1):
         self.states = np.empty((heigth, width), dtype=object)
-        self.rewards = np.empty((heigth, width), dtype=object)
+        self.q_val = np.empty((heigth, width), dtype=object)
+        self.rewards = np.empty((heigth, width), dtype=np.int16)
+
 
 
         for i in range (0, heigth):
             for j in range (0, width):
                 self.states[i][j] = State(reward, (i,j))
-                self.rewards[i][j] = self.states[i][j].rewards
+                self.q_val[i][j] = self.states[i][j].q_val
+                self.rewards[i][j] = self.states[i][j].reward
 
 
-    def change_reward(self, pos, direction, new_reward):
-        self.states[pos[0], pos[1]].change_reward(direction, new_reward)
-        self.rewards[pos[0], pos[1]] = self.states[pos[0], pos[1]].rewards
+    def change_q_val(self, pos, direction, new_reward):
+        self.states[pos[0], pos[1]].change_q_val(direction, new_reward)
+        self.q_val[pos[0], pos[1]] = self.states[pos[0], pos[1]].q_val
+
 
     def get_next_state(self, row, col, wind=True, explore=False):
         max_row = self.states.shape[0]
@@ -115,12 +119,32 @@ class MapState(object):
 
         return next_state
 
+    def show_map(self):
+        max_row = self.states.shape[0]
+        max_col = self.states.shape[1]
+        print('\n')
+        for row in range(0, max_row):
+            text_row=''
+            for col in range(0, max_col):
+                dir_mov = self.get_mov_dir(row, col)
+                #print('(',row,', ', col, ') - ', dir_mov)
+                if dir_mov == 0:
+                    text_row += 'UP\t'
+                elif dir_mov == 1:
+                    text_row += 'RIGHT\t'
+                elif dir_mov == 2:
+                    text_row += 'DOWN\t'
+                else:
+                    text_row += 'LEFT\t'
+            print(text_row)
+            print('\n')
+
     def get_mov_dir(self, row, col):
         max_row = self.states.shape[0]
         max_col = self.states.shape[1]
 
         state = self.states[row, col]
-        mov_dir = state.max_reward_idx()
+        mov_dir = state.max_q_val_idx()
 
         if mov_dir == 2 and (row+1 >= max_row):
             mov_dir = 0
